ce8411e25e7478206ed094accdf4cfe5a12741cc
Silas <faithsilas@gmail.com>
2022-02-12 19:00:57 -0500

    more explaination of 455;

--- Array/AssignCookies.py
+++ Array/AssignCookies.py
@@ -2,10 +2,10 @@
 class Solution:
     def findContentChildren(g: [int], s: [int]) -> int:
         kids = 0
-        g.sort() 
-        s.sort()
+        g.sort()  # O(n log n)
+        s.sort()  # O(n log n)
 
-        for c in s :    # check all cookies 
+        for c in s :    # check all cookies  O(n)
             if g[kids] <= c: # if the cookie can satisfy the child , then the kids +1 
                 kids += 1
             if kids == len(g): # if all kids already get a cooides then break the loop.
@@ -20,8 +20,11 @@ class Solution:
 
 
 '''
- Summary : 
+Summary : 
 First it needs to sort the list by ordering .
 use a two pointer to check all lists.
 the kids as a index for checking g-list, it move only the previous kid who are satisfied.
+
+time complexity : O(nlogn) + O(nlogn) + O(n) = O(ologn) 
+space complexity :O(1) because only need  a space to save the kids variable.
 '''
